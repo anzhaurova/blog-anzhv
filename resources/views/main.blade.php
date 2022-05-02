c705dbcf9bdabc540dd443b1d5887a041d404e71
victoria <vika.unit@gmail.com>
2022-05-02 20:57:41 +0300

    initial commit

---
+++ resources/views/main.blade.php
@@ -0,0 +1,166 @@
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+    <meta name="generator" content="Hugo 0.84.0">
+    <title>Anzhv blog</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/blog/">
+
+
+
+    <!-- Bootstrap core CSS -->
+    <link href="/docs/5.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <!-- Favicons -->
+    <link rel="apple-touch-icon" href="/docs/5.0/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+    <link rel="manifest" href="/docs/5.0/assets/img/favicons/manifest.json">
+    <link rel="mask-icon" href="/docs/5.0/assets/img/favicons/safari-pinned-tab.svg" color="#7952b3">
+    <link rel="icon" href="/docs/5.0/assets/img/favicons/favicon.ico">
+    <meta name="theme-color" content="#7952b3">
+
+
+    <style>
+        .bd-placeholder-img {
+            font-size: 1.125rem;
+            text-anchor: middle;
+            -webkit-user-select: none;
+            -moz-user-select: none;
+            user-select: none;
+        }
+
+        @media (min-width: 768px) {
+            .bd-placeholder-img-lg {
+                font-size: 3.5rem;
+            }
+        }
+    </style>
+
+
+    <!-- Custom styles for this template -->
+    <link href="https://fonts.googleapis.com/css?family=Playfair&#43;Display:700,900&amp;display=swap" rel="stylesheet">
+    <!-- Custom styles for this template -->
+{{--    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">--}}
+    <link rel="stylesheet" href="css/bootstrap.css" >
+</head>
+<body>
+
+<div class="container">
+    <header class="blog-header py-3">
+        <div class="row flex-nowrap justify-content-between align-items-center">
+            <div class="col-10 text-left">
+                <a class="blog-header-logo text-dark" href="#">ANZHV BLOG</a>
+            </div>
+            <div class="col-2 d-flex justify-content-end align-items-center">
+                <a class="link-secondary" href="#" aria-label="Search">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="mx-3" role="img" viewBox="0 0 24 24"><title>Search</title><circle cx="10.5" cy="10.5" r="7.5"/><path d="M21 21l-5.2-5.2"/></svg>
+                </a>
+                <a class="btn btn-sm btn-outline-secondary" href="#">Sign up</a>
+            </div>
+        </div>
+    </header>
+</div>
+
+<main class="container">
+
+    <div class="row g-5">
+        <div class="col-md-8">
+
+            <article class="blog-post">
+                <h2 class="blog-post-title">Programming books</h2>
+                <p class="blog-post-meta">April 27, 2022 </p>
+
+                <p>I like to read books, but I rarely read technical literature. Well, it's time to change it :). These books are essential books that people should have. I found it from this <a href="https://wiki.installgentoo.com/wiki/Programming_resources">programming_resources.</a> Sometimes I'll update this list.</p>
+
+
+                <h3>Recommended Reading for Developers:</h3>
+
+                <ol>
+                    <li>Learn You a Haskell for Great Good! by Lipovaca Miran</li>
+                    <li>Land of Lisp: Learn to Program in Lisp, One Game at a Time! by Conrad Barski</li>
+                    <li>Software Foundations by Benjamin C. Pierce</li>
+                    <li>Design Patterns: Elements of Reusable Object-Oriented Software by Erich Gamma</li>
+                    <li>Code: The Hidden Language of Computer Hardware and Software by Charles Petzold</li>
+                    <li>C Programming Language, 2nd Edition by Brian W. Kernighan</li>
+                    <li>The Seasoned Schemer, second edition (The MIT Press) by Daniel P. Friedman , Matthias Felleisen</li>
+                    <li>Real World Haskell by Bryan O'Sullivan, John Goerzen </li>
+                    <li>Algorithms (4th Edition) by Robert Sedgewick and Kevin Wayne </li>
+                    <li>The Algorithm Design Manual (Texts in Computer Science) by Steven S. Skiena</li>
+                    <li>The science of programming by David Gries</li>
+                    <li>Code Complete: A Practical Handbook of Software Construction, Second Edition
+                        Part of: Developer Best Practices (21 Books)  | by Steve McConnell </li>
+                    <li>Concepts, Techniques, and Models of Computer Programming (The MIT Press)
+                        by Peter Van Roy</li>
+                    <li>The Cathedral & the Bazaar: Musings on Linux and Open Source by an Accidental Revolutionary
+                        by Eric S. Raymond </li>
+                    <li>Algorithms and Data Structures by Niklaus Wirth </li>
+                    <li>Concrete Mathematics: A Foundation for Computer Science (2nd Edition)
+                        by Ronald Graham , Donald Knuth</li>
+                    <li>Compilers: Principles, Techniques, and Tools
+                        by Alfred Aho, Monica Lam, et al</li>
+                    <li>The Art of UNIX Programming (The Addison-Wesley Professional Computng Series)
+                        by Eric Raymond</li>
+                    <li>The Tao of Programming by Geoffrey James</li>
+                    <li>Hacker's Delight
+                        by Henry S. Warren Jr</li>
+                    <li>Structure and Interpretation of Computer Programs - 2nd Edition (MIT Electrical Engineering and Computer Science)
+                        Part of: MIT Electrical Engineering and Computer Science (2 Books)  | by Harold Abelson , Gerald Jay Sussman </li>
+                    <li>The Art of Computer Programming, Volumes 1-4A Boxed Set
+                        by Donald Knuth and Donald John Fuller</li>
+                    <li>Introduction to Algorithms, fourth edition
+                        by Thomas H. Cormen </li>
+                    <li>The Pragmatic Programmer: From Journeyman to Master
+                        by Andrew Hunt and David Thomas</li>
+                </ol>
+            </article>
+
+            <nav class="blog-pagination" aria-label="Pagination">
+                <a class="btn btn-outline-primary" href="#">Older</a>
+                <a class="btn btn-outline-secondary disabled" href="#" tabindex="-1" aria-disabled="true">Newer</a>
+            </nav>
+
+        </div>
+
+        <div class="col-md-4">
+            <div class="position-sticky" style="top: 2rem;">
+                <div class="p-4 mb-3 bg-light rounded">
+                    <h4 class="fst-italic">About me</h4>
+                    <p class="mb-0">👋 Hi! I'm Victoria. I do this blog for selfish reasons. I needed a way to keep track of my learning progress over time.
+                        I currently learning Laravel framework.
+                        Hopefully other people will find these posts helpful or interesting.💡
+                    </p>
+                </div>
+
+                <div class="p-4">
+                    <h4 class="fst-italic">Archives</h4>
+                    <ol class="list-unstyled mb-0">
+                        <li><a href="#">April 2022</a></li>
+                    </ol>
+                </div>
+
+                <div class="p-4">
+                    <h4 class="fst-italic">Elsewhere</h4>
+                    <ol class="list-unstyled">
+                        <li><a href="#">GitHub</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+<footer class="blog-footer" >
+{{--    <p>Blog template built for <a href="https://getbootstrap.com/">Bootstrap</a> by <a href="https://twitter.com/mdo">@mdo</a>.</p>--}}
+{{--    <p>--}}
+{{--        <a href="#">Back to top</a>--}}
+{{--    </p>--}}
+</footer>
+
+</body>
+</html>
